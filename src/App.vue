a5ddfab8f80b3e3d0569aabbff0462eb039c26f9
sasaki-yuta <wabisabi9024@gmail.com>
2022-12-17 00:56:20 +0900

    2022/12/17更新

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HeaderVue></HeaderVue>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderVue from './components/HeaderVue.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    HeaderVue
+}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+div {
+  border: 1px solid blue;
 }
 </style>
